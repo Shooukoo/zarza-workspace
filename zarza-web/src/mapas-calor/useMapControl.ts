e489a4384e86c224eb6fdd8fbf23a31b8c57398e
Shooukoo <snakexxs80@gmail.com>
2026-08-20 17:20:49 -0600

    feat: agregar hook useMapControl para controles Leaflet con portal de React

---
+++ zarza-web/src/mapas-calor/useMapControl.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import L from 'leaflet';
+import { useMap } from 'react-leaflet';
+
+export function useMapControl(position: L.ControlPosition): HTMLDivElement | null {
+  const map = useMap();
+  const [container, setContainer] = useState<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const control = new L.Control({ position });
+    control.onAdd = () => {
+      const div = L.DomUtil.create('div', 'leaflet-control');
+      L.DomEvent.disableClickPropagation(div);
+      setContainer(div);
+      return div;
+    };
+    map.addControl(control);
+
+    return () => {
+      map.removeControl(control);
+      setContainer(null);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [map, position]);
+
+  return container;
+}
